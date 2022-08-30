8c0fe7792e86e66de9bcc8fb6382eb710ded0f83
Fabian <FBosler@gmail.com>
2022-08-30 12:23:49 +0200

    first commit

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>Please navigate to "/test/xxx"</div>
+}
+
+export default Home
---
+++ pages/test/[[...slug]].js
@@ -0,0 +1,37 @@
+import { useTranslation } from 'next-i18next'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { useRouter } from 'next/router'
+
+export const getStaticPaths = async () => {
+  return {
+    paths: [], //indicates that no page needs be created at build time
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps = async (props) => {
+  const { locale = 'de' } = props
+
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ['common'])),
+    },
+    revalidate: 300,
+  }
+}
+
+const Component = () => {
+  const router = useRouter()
+  const { q, slug = [] } = router.query
+  const { t } = useTranslation('common')
+
+  return (
+    <>
+      Text depdending on locale should be here - Title:{' '}
+      <span style={{ color: 'red', marginTop: '20px' }}>{t('title')}</span>
+      <p>The url query params are: {slug}</p>
+    </>
+  )
+}
+
+export default Component
